32288c28cb78dead317c8d76f0948034e17e3703
GregoryWTz <gwtlibra@gmail.com>
2026-09-06 14:25:45 +0700

    Simple Linear Regression done

--- Week2/SimpleLinearR.py
+++ Week2/SimpleLinearR.py
@@ -9,11 +9,11 @@ area = 2.5 * np.random.randn(100) + 25
 price = 25 * area + 5 + np.random.randint(20,50, size = len(area))
 data = np.array([area, price])
 data = pd.DataFrame(data.T, columns = ['area', 'price'])
-plt.scatter(data['area'], data['price'])
-plt.xlabel('Area')
-plt.ylabel('Price')
-plt.title('Example Data')
-plt.show()
+# plt.scatter(data['area'], data['price'])
+# plt.xlabel('Area')
+# plt.ylabel('Price')
+# plt.title('Example Data')
+# plt.show()
 
 #Calculating the two regression coefficients using the equations
 W = sum(price*(area - np.mean(area))) / sum((area - np.mean(area))**2)
@@ -21,3 +21,15 @@ b = np.mean(price) - W * np.mean(area)
 print("======================================")
 print("The calculated regression coefficients are: ", W, b)
 print("======================================\n")
+
+#predicting the price using the calculated coefficients
+y_predicted_price = W * area + b
+
+#Plotting the predicted price against the actual price
+plt.plot(area, y_predicted_price, color = 'red', label = 'Predicted Price')
+plt.scatter(data['area'], data['price'], label = 'Actual Price')
+plt.xlabel('Area')
+plt.ylabel('Price')
+plt.legend()
+plt.title('Predicted vs Actual Price')
+plt.show()
